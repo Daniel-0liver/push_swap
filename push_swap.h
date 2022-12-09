349becd74e9714a362c893a8d215ebe5f5eeff2a
Daniel-0liver <dtoliverbr@gmail.com>
2022-12-09 01:42:35 +0000

    start

---
+++ push_swap.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/08 23:46:01 by dateixei          #+#    #+#             */
+/*   Updated: 2022/12/08 23:55:45 by dateixei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP
+# define PUSH_SWAP
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct d_stacks
+{
+	int		*stack1;
+	int		*stack2;
+}              t_stacks;
+
+
+#endif
