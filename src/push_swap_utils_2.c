d19b7c523c4577b3787ff0741f7c168414352907
Daniel-0liver <dtoliverbr@gmail.com>
2022-12-22 01:08:21 +0000

    choise amount of minimum numbers

--- src/push_swap_utils_2.c
+++ src/push_swap_utils_2.c
@@ -6,7 +6,7 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 00:52:22 by dateixei          #+#    #+#             */
-/*   Updated: 2022/12/21 01:36:01 by dateixei         ###   ########.fr       */
+/*   Updated: 2022/12/22 01:05:15 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,8 @@ void	find_biggest_nbr(t_stacks *stack)
 
 	i = 0;
 	stack->nbr_biggest = stack->stack_a[0];
-	while (i < stack->size_stack_a -1)
+	stack->nbr_biggest = 0;
+	while (i < stack->size_stack_a -1 && stack->size_stack_a > 0)
 	{
 		if (stack->stack_a[i] >= stack->nbr_biggest)
 		{
@@ -70,7 +71,8 @@ void	find_lowest_nbr(t_stacks *stack)
 
 	i = 0;
 	stack->nbr_lowest = stack->stack_a[0];
-	while (i < stack->size_stack_a -1)
+	stack->posit_lowest = 0;
+	while (i < stack->size_stack_a && stack->size_stack_a > 0)
 	{
 		if (stack->stack_a[i] <= stack->nbr_lowest)
 		{
@@ -89,27 +91,29 @@ void	find_second_lowest_nbr(t_stacks *stack)
 	int nbr_count[SIZE_NBR];
 	int posit_count[SIZE_NBR];
 
-	j = 0;
-	stack->nbr_lowest = stack->stack_a[0];
-	stack->posit_lowest = 0;
+	find_lowest_nbr(stack);
+	find_biggest_nbr(stack);
+	nbr_count[0] = stack->nbr_lowest;
+	posit_count[0] = stack->posit_lowest;
+	j = 1;
+	i = 0;
 	while (j < SIZE_NBR)
-		nbr_count[j++] = stack->stack_a[0];
-	k = 0;
-	while (k < SIZE_NBR)
+		nbr_count[j++] = stack->nbr_biggest;
+	k = 1;
+	while (k < SIZE_NBR && stack->size_stack_a > 1)
 	{
+		stack->nbr_lowest = stack->nbr_biggest;
+		stack->posit_lowest = stack->posit_biggest;
 		i = 0;
-		while (i < stack->size_stack_a - 1 && stack->size_stack_a > 1)
+		while (i < stack->size_stack_a)
 		{
 			j = 0;
-			while (j < SIZE_NBR && stack->stack_a[i] != stack->nbr_lowest)
+			while (j < SIZE_NBR)
 			{
-				if (stack->stack_a[i] < stack->nbr_lowest)
+				if (stack->stack_a[i] > nbr_count[k - 1] && stack->stack_a[i] < stack->nbr_lowest)
 				{
-					if (stack->stack_a[i] != stack->nbr_lowest)
-					{
-						stack->nbr_lowest = stack->stack_a[i];
-						stack->posit_lowest = i;
-					}
+					stack->nbr_lowest = stack->stack_a[i];
+					stack->posit_lowest = i;
 					printf("nbr = %d, posit = %d\n", stack->nbr_lowest, stack->posit_lowest);
 				}
 				j++;
@@ -120,12 +124,36 @@ void	find_second_lowest_nbr(t_stacks *stack)
 		posit_count[k] = stack->posit_lowest;
 		k++;
 	}
+	k = 0;
+	j = 0;
+	if (((stack->size_stack_a - 1) - posit_count[k]) < posit_count[k])
+		i = (stack->size_stack_a - 1) - posit_count[k];
+	else
+		i = posit_count[k];
+	while (k < SIZE_NBR)
+	{
+		if (((stack->size_stack_a - 1) - posit_count[k]) < posit_count[k] && ((stack->size_stack_a - 1) - posit_count[k]) < i)
+		{
+			i = (stack->size_stack_a - 1) - posit_count[k];
+			printf("%d\n", i);
+			j = k;
+		}
+		else if (posit_count[k] < i)
+		{
+			i = posit_count[k];
+			j = k;
+		}
+		k++;
+	}
+	stack->nbr_lowest = nbr_count[j];
+	stack->posit_lowest = posit_count[j];
 	i = 0;
 	while (i < SIZE_NBR)
 	{
 		printf("nbr_count[%d] = %d, posit_count[%d] = %d\n", i, nbr_count[i], i, posit_count[i]);
 		i++;
 	}
+	printf("low = %d, posit = %d\n", stack->nbr_lowest, stack->posit_lowest);
 	exit(0);
 }
 
